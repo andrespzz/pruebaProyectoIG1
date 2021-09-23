4afcfb2bfd4bfbf59c82b398f3e31b2947e43680
Julia <juliajimenezayuso72@gmail>
2021-09-23 12:21:43 +0200

    Cambios Modificar

--- src/main/java/pp/view/PModificar.java
+++ src/main/java/pp/view/PModificar.java
@@ -1,5 +1,22 @@
 package pp.view;
 
-public class PModificar {
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+
+public class PModificar extends JPanel {
+	private JTextField txtApellidos;
+	private JTextField txtNombrePersona;
+	private JTextField txtDniPersona;
+	private JTextField txtFechanac;
+	private JSpinner spnEdad;
+	public PModificar() {
+		initComponents();
+	}
+
+	private void initComponents() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
